998d3082be170f3a5b9aa27d3afa3aa04a692685
Alex Demyankov <alex.demyankov@gmail.com>
2020-07-24 18:40:17 -0700

    Refactor transition matrix & add key/value tests

--- include/mjson/mjson.hpp
+++ include/mjson/mjson.hpp
@@ -13,22 +13,20 @@ namespace mjson {
 class json {
 public:
     json(const std::string& s) : s_(s), pos_(0), state_(0), is_done_(false) {
-        // Json starts
-        pos_ = s_.find('{');
-        if (pos_ == std::string::npos) return;
+        if (s_.empty()) return;
 
-        // Parser
-        while (++pos_ < s_.length()) {
+        do {
             char op = transition_[state_][dictionary_[s_.at(pos_)]];
             if (op < 0) {
-                // Format error or done parsing
                 is_done_ = op == -2 ? true: false;
                 return;
             }
 
-            if (actions_[op]) (this->*actions_[op])();
+            Action f = actions_[op];
+            if (f) if (!(this->*f)()) return;
+
             state_ = op & 0x0f;
-        }
+        } while(++pos_ < s_.length());
     }
 
     json() = default;
@@ -61,7 +59,7 @@ private:
     using KeyValueMap = std::map<std::string, std::string>;
     using KeyArrayMap = std::map<std::string, Array>;
     using Dictionary = std::array<char, 126>;
-    using Action = void (json::*)();
+    using Action = bool (json::*)();
     using Actions = std::array<Action, 0x31>;
 
     std::string k_{};
@@ -74,39 +72,54 @@ private:
     KeyArrayMap kam_{};
     KeyValueMap kvm_{};
 
-    void onKeyChar() { k_ += s_.at(pos_); }
-    void onValueChar() { v_ += s_.at(pos_); }
-    void onArrayBegin() { is_array_ = true; }
+    bool onKeyChar() {
+        k_ += s_.at(pos_);
+        return true;
+    }
 
-    void onObjectBegin() {
+    bool onValueChar() {
+        v_ += s_.at(pos_);
+        return true;
+    }
+
+    bool onArrayBegin() {
+        is_array_ = true;
+        return true;
+    }
+
+    bool onObjectBegin() {
         json obj(s_.data() + pos_);
         if (obj.is_valid()) {
             kom_.insert(KeyObjectMap::value_type(k_, std::move(obj)));
         }
+        return true;
     }
 
-    void onArrayItem() {
+    bool onArrayItem() {
         array_.push_back(v_);
         v_.clear();
+        return true;
     }
 
-    void onArrayDone() {
+    bool onArrayDone() {
         is_array_ = false;
         kam_.insert(KeyArrayMap::value_type(k_, array_));
         array_.clear();
         k_.clear();
     }
 
-    void onKVPairDone() {
+    bool onKVPairDone() {
         if (is_array_) {
             onArrayItem();
             state_ = 3;
-            return;
+            return true;
         }
 
         kvm_.insert(KeyValueMap::value_type(k_, v_));
         k_.clear();
         v_.clear();
+
+        return true;
     }
 
     static constexpr Dictionary dictionary_ = []() {
@@ -115,63 +128,98 @@ private:
         dic[' '] = 1;
         dic['\t'] = 2;
         dic['\n'] = 3;
-        dic['"'] = 4;
-        dic[':'] = 5;
-        dic[','] = 6;
-        dic['{'] = 7;
-        dic['}'] = 8;
-        dic['['] = 9;
-        dic[']'] = 10;
+        dic['\r'] = 4;
+        dic['"'] = 5;
+        dic[':'] = 6;
+        dic[','] = 7;
+        dic['{'] = 8;
+        dic['}'] = 9;
+        dic['['] = 10;
+        dic[']'] = 11;
 
         return dic;
     }();
 
-    const Actions actions_ = []() {
-        Actions h{};
+    //const Actions actions_ = []() {
+    //    Actions h{};
 
-        h[0x10] = &json::onKVPairDone;
-        h[0x11] = &json::onKeyChar;
-        h[0x14] = &json::onValueChar;
-        h[0x15] = &json::onValueChar;
-        h[0x13] = &json::onArrayBegin;
-        h[0x23] = &json::onArrayDone;
-        h[0x30] = &json::onObjectBegin;
+    //    h[0x10] = &json::onKVPairDone;
+    //    h[0x11] = &json::onKeyChar;
+    //    h[0x14] = &json::onValueChar;
+    //    h[0x15] = &json::onValueChar;
+    //    h[0x13] = &json::onArrayBegin;
+    //    h[0x23] = &json::onArrayDone;
+    //    h[0x30] = &json::onObjectBegin;
 
-        return h;
-    }();
+    //    return h;
+    //}();
 
     // Transition matrix. A primitive state machine with json parsing rules
     // 0xXX - XX byte is an action, where :4 lower bits is a transition to a state;
     // -1 is a format error; -2 is done parsing
-    static constexpr char transition_[6][11] = {
-        //        code (dictionary)
-        //         0     1     2     3     4     5     6     7     8     9    10
-        //         *    ' '   \t    \n     "     :     ,     {     }     [     ]      state (transition)
-        /*0*/ {   -1,    0,    0,    0,    1,   -1,    3,   -1,   -2,   -1, 0x23 },   // 0 - Main loop: Key Value pair starts/ends
-        /*1*/ { 0x11,   -1,   -1,   -1,    2,   -1,   -1,   -1,   -1,   -1,   -1 },   // 1 - Key Name assembling
-        /*2*/ {   -1,    2,    2,    2,   -1,    3,   -1,   -1,   -1,   -1,   -1 },   // 2 - Value/Array/Object begins
-        /*3*/ { 0x14,    3,    3,    0,    5,   -1,    0, 0x30,   -2, 0x13, 0x23 },   // 3 - Whitespase between dictionary chars
-        /*4*/ { 0x14, 0x10, 0x10, 0x10,   -1,   -1, 0x10,   -1,   -1,   -1,   -1 },   // 4 - Value String assembling (w/o whitespace chars)
-        /*5*/ { 0x15, 0x15, 0x15, 0x15, 0x10, 0x15, 0x15,   -1, 0x15, 0x15, 0x15 }    // 5 - Value String assembling (with all chars available)
-    };
-
-
-    //static constexpr char tr_[6][11] = {
+    //static constexpr char transition_[6][11] = {
     //    //        code (dictionary)
     //    //         0     1     2     3     4     5     6     7     8     9    10
-    //    //         *    ' '   \t    \n     "     :     ,     {     }     [     ]     state (transition)
-    //    /*X*/ {   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1 }, // X - 
-
-    //    /*0*/ {   -1,    0,    0,    0,   -1,   -1,   -1,    1,   -1,   -1,   -1 }, // 0 - Json header
-    //    /*1*/ {   -1,    1,    1,    1, 0x12,   -1,   -1,   -1,   -2,   -1,   -1 }, // 1 - Main loop: 12 - key start
-    //    /*2*/ { 0x22,   -1,   -1,   -1, 0x13,   -1,   -1,   -1,   -1,   -1,   -1 }, // 3 - Key; 22 - consume 13 - end
-    //    /*3*/ {   -1,    3,    3,    3,   -1, 0x14,   -1,   -1,   -1,   -1,   -1 }, // 3 - Value; 14 - start
-    //    /*4*/ { 0x16,    4,    4,    4,    5,   -1,   -1,   -1,   -1,   -1,   -1 }, // 4 - Value; split to 5 or 6
-    //    /*5*/ { 0x15, 0x15, 0x15, 0x15, 0x31, 0x15, 0x15, 0x15, 0x15, 0x15, 0x15 }, // 5 - Value; 5 - string in quotes ""; 15,16 - consume
-    //    /*6*/ { 0x16, 0x31,   -1,   -1,   -1,   -1, 0x31,   -1,   -1,   -1,   -1 }, // 6 - Value; 6 - start string w/o quotes "" 31 - end
+    //    //         *    ' '   \t    \n     "     :     ,     {     }     [     ]      state (transition)
+    //    /*0*/ {   -1,    0,    0,    0,    1,   -1,    3,   -1,   -2,   -1, 0x23 },   // 0 - Main loop: Key Value pair starts/ends
+    //    /*1*/ { 0x11,   -1,   -1,   -1,    2,   -1,   -1,   -1,   -1,   -1,   -1 },   // 1 - Key Name assembling
+    //    /*2*/ {   -1,    2,    2,    2,   -1,    3,   -1,   -1,   -1,   -1,   -1 },   // 2 - Value/Array/Object begins
+    //    /*3*/ { 0x14,    3,    3,    0,    5,   -1,    0, 0x30,   -2, 0x13, 0x23 },   // 3 - Whitespase between dictionary chars
+    //    /*4*/ { 0x14, 0x10, 0x10, 0x10,   -1,   -1, 0x10,   -1,   -1,   -1,   -1 },   // 4 - Value String assembling (w/o whitespace chars)
+    //    /*5*/ { 0x15, 0x15, 0x15, 0x15, 0x10, 0x15, 0x15,   -1, 0x15, 0x15, 0x15 }    // 5 - Value String assembling (with all chars available)
+    //};
 
 
-    //}
+    //        code (dictionary)
+    //         0     1     2     3     4     5     6     7     8     9     a     b
+    //         *    ' '   \t    \n    \r     "     :     ,     {     }     [     ]     state (transition)
+  /*X*/ //{   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1,   -1 }, // X - 
+
+    static constexpr char transition_[7][12] = {
+    /*0*/ {   -1,    0,    0,    0,    0,   -1,   -1,   -1,    1,   -1,   -1,   -1 }, // 0 - Json header
+    /*1*/ {   -1,    1,    1,    1,    1, 0x12,   -1,   -1,   -1,   -2,   -1,   -1 }, // 1 - Main loop: 12 - key start
+    /*2*/ { 0x22, 0x22,   -1,   -1,   -1, 0x13, 0x22, 0x22, 0x22, 0x22, 0x22, 0x22 }, // 2 - Key; 22 - consume 13 - end
+    /*3*/ {   -1,    3,    3,    3,    3,   -1,    4,   -1,   -1,   -1,   -1,   -1 }, // 3 - ':' separator expected
+    /*4*/ {   -1,    4,    4,    4,    4, 0x15,   -1,   -1,   -1,   -1,   -1,   -1 }, // 4 - Value; 15 - start
+    /*5*/ { 0x25, 0x25,   -1,   -1,   -1, 0x16, 0x25, 0x25, 0x25, 0x25, 0x25, 0x25 }, // 5 - Value; 25 - consume 16 - end
+    /*6*/ {   -1,    6,    6,    6,    6,   -1,   -1,    1,   -1,   -2,   -1,   -1 }, // 6 - Is there next pair?
+
+
+    };
+
+    bool onKeyStart() { return true; }
+    bool onValueStart() { return true; }
+    bool onKeyEnd() { return kvm_.find(k_) == kvm_.end() ? true : false; }
+
+    bool onValueEnd() {
+        kvm_.insert(KeyValueMap::value_type(k_, v_));
+        k_.clear();
+        v_.clear();
+
+        return true;
+    }
+
+    const Actions actions_ = []() {
+        Actions h{};
+
+        h[0x12] = &json::onKeyStart;
+        h[0x13] = &json::onKeyEnd;
+        h[0x15] = &json::onValueStart;
+        h[0x16] = &json::onValueEnd;
+        h[0x22] = &json::onKeyChar;
+        h[0x25] = &json::onValueChar;
+
+        //h[] = &json::onHeaderEnd;
+        //h[] = &json::onKVPairDone;
+        //h[] = &json::onKeyChar;
+        //h[] = &json::onValueChar;
+        //h[] = &json::onValueChar;
+        //h[] = &json::onArrayBegin;
+        //h[] = &json::onArrayDone;
+        //h[] = &json::onObjectBegin;
+
+        return h;
+    }();
 };
 
 } // namespace tiny
--- test/mjson_test/src/mjson_test.cpp
+++ test/mjson_test/src/mjson_test.cpp
@@ -1,4 +1,4 @@
-#define CATCH_CONFIG_MAIN
+﻿#define CATCH_CONFIG_MAIN
 #define _SILENCE_CXX17_UNCAUGHT_EXCEPTION_DEPRECATION_WARNING
 
 #include "catch.hpp"
@@ -6,34 +6,236 @@
 #include <mjson/mjson.hpp>
 using namespace mjson;
 
-TEST_CASE("Empty string", "[json]") {
+TEST_CASE("Empty string", "[header]") {
     json js("");
     REQUIRE_FALSE(js.is_valid());
 }
 
-TEST_CASE("Not a valid json string", "[json]") {
+TEST_CASE("Not a valid json string", "[header]") {
     const auto in = R"(*)";
 
     json js(in);
     REQUIRE_FALSE(js.is_valid());
 }
 
-TEST_CASE("Not a valid json body", "[json]") {
-    const auto in = R"(
-        {
-    )";
+TEST_CASE("Not a valid json body", "[header]") {
+    SECTION("Partial") {
+        const auto in = R"(
+            {
+        )";
 
-    json js(in);
-    REQUIRE_FALSE(js.is_valid());
+        json js(in);
+        REQUIRE_FALSE(js.is_valid());
+    }
+
+    SECTION("Illegal symbol") {
+        const auto in = R"(
+            ,{}
+        )";
+
+        json js(in);
+        REQUIRE_FALSE(js.is_valid());
+    }
+
+    SECTION("No opening curly brace") {
+        const auto in = R"(
+            "name" : value
+        )";
+
+        json js(in);
+        REQUIRE_FALSE(js.is_valid());
+    }
+
+    SECTION("Not valid") {
+        const auto in = R"(
+            }{
+        )";
+
+        json js(in);
+        REQUIRE_FALSE(js.is_valid());
+    }
 }
 
-TEST_CASE("A valid json body", "[json]") {
-    const auto in = R"(
-        {
-        }
-    )";
+TEST_CASE("A valid json body", "[header]") {
+    SECTION("Valid") {
+        const auto in = R"(
+            {
+            }
+        )";
 
-    json js(in);
-    REQUIRE(js.is_valid());
+        json js(in);
+        REQUIRE(js.is_valid());
+    }
+
+    SECTION("Valid /w escapes") {
+        const auto in = "\n{ \t \n \r }\n";
+
+        json js(in);
+        REQUIRE(js.is_valid());
+    }
+}
+
+TEST_CASE("Invalid key name string", "[key]") {
+    SECTION("Opening quote is expected") {
+        const auto in = R"(
+            {
+                name" : ""
+            }
+        )";
+
+        json js(in);
+        REQUIRE_FALSE(js.is_valid());
+    }
+
+    SECTION("Closing quote is expected") {
+        const auto in = R"(
+            {
+                "name : ""
+            }
+        )";
+
+        json js(in);
+        REQUIRE_FALSE(js.is_valid());
+    }
+
+    SECTION("Invalid quote symbols") {
+        const auto in = R"(
+            {
+                'name' : ""
+            }
+        )";
+
+        json js(in);
+        REQUIRE_FALSE(js.is_valid());
+    }
+
+    SECTION("Illigal LF in the name") {
+        const auto in = R"(
+            {
+                "Na
+me" : ""
+            }
+        )";
+
+        json js(in);
+        REQUIRE_FALSE(js.is_valid());
+    }
+
+    SECTION("Illigal vertical tab in the name") {
+        const auto in = R"(
+            {
+                "na	me" : ""
+            }
+        )";
+
+        json js(in);
+        REQUIRE_FALSE(js.is_valid());
+    }
+}
+
+TEST_CASE("Valid key name", "[key]") {
+    SECTION("key1") {
+        const auto in = R"(
+            {
+                "name" : ""
+            }
+        )";
+
+        json js(in);
+        REQUIRE(js.is_valid());
+        REQUIRE(js.size() == 1);
+        REQUIRE(js["name"] == "");
+    }
+
+    SECTION("key2") {
+        const auto in = R"(
+            {
+                "Key Name" : ""
+            }
+        )";
+
+        json js(in);
+        REQUIRE(js.is_valid());
+        REQUIRE(js.size() == 1);
+        REQUIRE(js["Key Name"] == "");
+    }
+
+    SECTION("key3") {
+        const auto in = R"(
+            {
+                "Name" : ""
+            }
+        )";
+
+        json js(in);
+        REQUIRE(js.is_valid());
+        REQUIRE(js.size() == 1);
+        REQUIRE(js["Name"] == "");
+    }
+
+    SECTION("key4") {
+        const auto in = R"(
+            {
+                "LongName-TheJsonKey" : ""
+            }
+        )";
+
+        json js(in);
+        REQUIRE(js.is_valid());
+        REQUIRE(js.size() == 1);
+        REQUIRE(js["LongName-TheJsonKey"] == "");
+    }
+
+    SECTION("key5") {
+        const auto in = R"(
+            {
+                "Name123" : ""
+            }
+        )";
+
+        json js(in);
+        REQUIRE(js.is_valid());
+        REQUIRE(js.size() == 1);
+        REQUIRE(js["Name123"] == "");
+    }
+
+    SECTION("Empty string") {
+        const auto in = R"(
+            {
+                "" : ""
+            }
+        )";
+
+        json js(in);
+        REQUIRE(js.is_valid());
+        REQUIRE(js.size() == 1);
+        REQUIRE(js[""] == "");
+    }
+}
+
+TEST_CASE("Not a valid value separator", "[value]") {
+    SECTION("No colon") {
+        const auto in = R"(
+            {
+                "name" value
+            }
+        )";
+
+        json js(in);
+        REQUIRE_FALSE(js.is_valid());
+        REQUIRE(js.size() == 0);
+    }
+
+    SECTION("Invalid separator") {
+        const auto in = R"(
+            {
+                "name" ; value
+            }
+        )";
+
+        json js(in);
+        REQUIRE_FALSE(js.is_valid());
+        REQUIRE(js.size() == 0);
+    }
 }
 
